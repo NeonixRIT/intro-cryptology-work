e223c38ce19214b6d97ec90657af890c00b6cf60
NeonixRIT <kamron.cole13@gmail.com>
2024-11-21 15:53:57 -0500

    fix python_hash_slice hashing 0s as None

--- Modernish/pyhash.py
+++ Modernish/pyhash.py
@@ -4,7 +4,7 @@ from sys import byteorder
 from typing import Any
 
 from math import frexp, isinf, isfinite
-from sys import maxsize
+from sys import maxsize, version_info
 
 """
 Reference: https://github.com/python/cpython
@@ -317,9 +317,7 @@ def siphash13(k0: int, k1: int, src: bytes) -> int:
     v0, v1, v2, v3 = single_round(v0, v1, v2, v3)
     v0, v1, v2, v3 = single_round(v0, v1, v2, v3)
 
-    # print(bin(v0)[2:].zfill(64), bin(v1)[2:].zfill(64), bin(v2)[2:].zfill(64), bin(v3)[2:].zfill(64), '', sep='\n')
-    t = (v0 ^ v1) ^ (v2 ^ v3)
-    return _u64tos(t)
+    return (v0 ^ v1) ^ (v2 ^ v3)
 
 
 def siphash24(k0: int, k1: int, src: bytes) -> int:
@@ -357,8 +355,7 @@ def siphash24(k0: int, k1: int, src: bytes) -> int:
     v0, v1, v2, v3 = double_round(v0, v1, v2, v3)
     v0, v1, v2, v3 = double_round(v0, v1, v2, v3)
 
-    t = (v0 ^ v1) ^ (v2 ^ v3)
-    return _u64tos(t)
+    return (v0 ^ v1) ^ (v2 ^ v3)
 
 
 # Hash functions for different types
@@ -370,7 +367,11 @@ def python_hash_integer(data: int) -> int:
     """
     if data < 0:
         return -((-data) % 0x1FFFFFFFFFFFFFFF)
-    return data % 0x1FFFFFFFFFFFFFFF
+    return _u64tos(data % 0x1FFFFFFFFFFFFFFF)
+
+
+def python_hash_boolean(data: bool) -> int:
+    return python_hash_integer(int(data))
 
 
 def djbx33a(data: bytes, suffix: bytes) -> int:
@@ -383,12 +384,11 @@ def djbx33a(data: bytes, suffix: bytes) -> int:
         h = ((h << 5) + h) + byte
     h ^= len(data)
     h ^= int.from_bytes(suffix, 'little')
-    # h = h if h & 0x8000000000000000 == 0 else h - 0x10000000000000000
-    return _u64tos(h)
+    return h
 
 
-def python_hash_pointer(pointer: int):
-    return rotr64(pointer, 4)
+def python_hash_pointer(data: object):
+    return _u64tos(rotr64(id(data), 4))
 
 
 def python_hash_bytes(data: bytes):
@@ -396,9 +396,9 @@ def python_hash_bytes(data: bytes):
     if len(data) == 0:
         return 0
     if len(data) < CUTOFF:  # use separate function for small strings
-        return djbx33a(data, U[16:])
+        return _u64tos(djbx33a(data, U[16:]))
     else:
-        return siphash13(_le64toh(int.from_bytes(U[:8], 'little')), _le64toh(int.from_bytes(U[8:16], 'little')), data)
+        return _u64tos(siphash13(_le64toh(int.from_bytes(U[:8], 'little')), _le64toh(int.from_bytes(U[8:16], 'little')), data))
 
 
 def python_hash_string(data: str):
@@ -462,26 +462,87 @@ def python_hash_tuple(data: tuple) -> int:
     return _u64tos(acc)
 
 
+def python_hash_range(data: range) -> int:
+    length = len(data)
+    start = data.start if length > 0 else None
+    step = data.step if length > 1 else None
+    t = (length, start, step)
+    return python_hash_tuple(t)
+    # if length == 0:
+    # else:
+    #     t = (length, data.start, None)
+    #     if length > 1:
+    #         t = (length, data.start, data.step)
+    #     else:
+    #         t = (length, data.start, None)
+    #     return python_hash_tuple(t)
+    # start = data.start if length > 0 else None
+    # step = data.step if length > 0 else None
+
+
+def python_hash_slice_part(com: int | None, acc: int):
+    lane = python_hash_integer(com) if com is not None else python_hash_none(com)
+    if lane == -1:
+        return -1
+    acc = (acc + ((lane * HASH_XXPRIME_2) & 0xFFFFFFFFFFFFFFFF)) & 0xFFFFFFFFFFFFFFFF
+    acc = HASH_XXROTATE(acc)
+    acc = (acc * HASH_XXPRIME_1) & 0xFFFFFFFFFFFFFFFF
+    return acc
+
+
+def python_hash_slice(data: slice) -> int:
+    '''
+    TODO: FIX calc for slices:
+        slice(None, 0, None)
+        slice(0, 14, None)
+        slice(0, 16, None)
+        slice(0, 19, 1)
+        slice(0, 19, 4)
+    '''
+    if version_info < (3, 12, 0):
+        raise NotImplementedError('hashing slice objects is not supported in Python versions < 3.12.0')
+    acc = HASH_XXPRIME_5
+    acc = python_hash_slice_part(data.start, acc)
+    acc = python_hash_slice_part(data.stop, acc)
+    acc = python_hash_slice_part(data.step, acc)
+    if acc == -1:
+        return 1546275796
+    return _u64tos(acc)
+
+
+def python_hash_memoryview(data: memoryview) -> int:
+    if not data.readonly:
+        raise ValueError('cannot hash writable memoryview object')
+
+    if data.format != 'B' and data.format != 'b' and data.format != 'c':
+        raise ValueError("memoryview: hashing is restricted to formats 'B', 'b', or 'c'")
+
+    if data.obj is not None and python_hash(data.obj) == -1:
+        return -1
+
+    return python_hash_bytes(data.tobytes())
+
+
+def python_hash_none(data: None) -> int:
+    if version_info < (3, 12, 0):
+        return _u64tos(python_hash_pointer(data))
+    return _u64tos(0xFCA86420)
+
+
 # Main hash function
 def python_hash(data: Any) -> int:
     """
     main function to hash data.
 
     does not respect object's __hash__ functions
-    but does respect __pyhash__ functions
-        __pyhash__ must return a representation of the object in a base hashable type
-        This should be the same state used in an object's __eq__
 
     Does not currently handle slice, range, or memoryview objects
         - range hash function creates a tuple of 3 (length, start, step) with logic setting each to None accordingly
         - slice hash function similar to tuple uses modified xxHash on start, stop, and step
         - memoryview hash function can only hash readonly memoryviews in formats B, b, or c, else, hashes buffer (bytes)
     """
-    # respect __pyhash__ functions
-    if getattr(data, '__pyhash__', None) is not None:
-        data = data.__pyhash__()
-        hash_value = python_hash(data)
-        return -2 if hash_value == -1 else hash_value
+    if data is None:
+        return python_hash_none(data)
 
     # unhashable types raise error
     # isinstance(data, (list, dict, set, bytearray, memoryview))
@@ -489,9 +550,12 @@ def python_hash(data: Any) -> int:
         raise TypeError(f'unhashable type: {type(data).__name__}')
 
     # Hashable base types
+    # TODO replace with dictionary
     hash_value = 0
     if isinstance(data, int):
         hash_value = python_hash_integer(data)
+    elif isinstance(data, bool):
+        hash_value = python_hash_boolean(data)
     elif isinstance(data, bytes):
         hash_value = python_hash_bytes(data)
     elif isinstance(data, str):
@@ -500,8 +564,14 @@ def python_hash(data: Any) -> int:
         hash_value = python_hash_double(data)
     elif isinstance(data, tuple):
         hash_value = python_hash_tuple(data)
+    elif isinstance(data, range):
+        hash_value = python_hash_range(data)
+    elif isinstance(data, slice):
+        hash_value = python_hash_slice(data)
+    elif isinstance(data, memoryview):
+        hash_value = python_hash_memoryview(data)
     elif isinstance(data, object):
-        hash_value = python_hash_pointer(id(data))
+        hash_value = python_hash_pointer(data)
     else: # should never be reached since all data types should be objects
         raise TypeError(f'unhashable type: {type(data).__name__}')
 
@@ -527,15 +597,39 @@ def main():
         def __str__(self):
             return f'TestObject({self.name=}, ptr={id(self)})'
 
+    sys_rand = SystemRandom()
     rand_string = random_string(20)
     rand_bytes = random_string(20).encode()
-    rand_int = SystemRandom().randint(0, 1000000000)
-    rand_double = SystemRandom().random()
+    rand_int = sys_rand.randint(0, 1000000000)
+    rand_double = sys_rand.random()
     rand_obj = TestObject(rand_string)
-    rand_tup = tuple([SystemRandom().randint(0, 11) for _ in range(SystemRandom().randint(0, 11))])
+    rand_tup = tuple([sys_rand.randint(0, 11) for _ in range(sys_rand.randint(0, 11))])
+    rand_range1 = range(sys_rand.randint(0, 11))
+    rand_range2 = range(sys_rand.randint(0, 11), sys_rand.randint(11, 22))
+    rand_range3 = range(sys_rand.randint(0, 11), sys_rand.randint(11, 22), sys_rand.randint(1, 5))
+    none = None
+    bool_t = True
+    bool_f = False
+    rand_slice1 = slice(sys_rand.randint(0, 11))
+    rand_slice2 = slice(sys_rand.randint(0, 11), sys_rand.randint(11, 22))
+    rand_slice3 = slice(sys_rand.randint(0, 11), sys_rand.randint(11, 22), sys_rand.randint(1, 5))
+    norm_slice1 = slice(None, None, None),
+    norm_slice2 = slice(0, None, None),
+    norm_slice3 = slice(None, 0, None),
+    norm_slice4 = slice(None, None, 0),
+    rand_memoryview = memoryview(random_string(20).encode())
 
-    things_to_hash = [rand_string, rand_bytes, rand_int, rand_double, rand_tup, rand_obj]
-    print(things_to_hash)
+    things_to_hash = [
+        rand_string, rand_bytes, rand_int,
+        rand_double, rand_tup, rand_obj,
+        rand_range1, rand_range2, rand_range3,
+        none, bool_t, bool_f,
+        rand_slice1, rand_slice2, rand_slice3,
+        norm_slice1, norm_slice2, norm_slice3,
+        norm_slice4, rand_memoryview
+    ]
+
+    print(things_to_hash, '\n')
     for obj in things_to_hash:
         res1 = python_hash(obj)
         res2 = hash(obj)
@@ -544,6 +638,7 @@ def main():
         print(f'\tComputed hash : {res1}')
         print(f'\tPython hash   : {res2}')
         print()
+        assert res1 == res2, f'Hashes do not match for {obj=}'
 
 
 if __name__ == '__main__':
